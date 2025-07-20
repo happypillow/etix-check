605a8497050a8523a208a8acbb25e0e2f0db0e4d
happypillow <h.mountainpaddy@gmail.com>
2025-07-20 22:04:26 +0900

    Update check_etix_ticket.py

--- check_etix_ticket.py
+++ check_etix_ticket.py
@@ -1,13 +1,12 @@
 import os
 import requests
 
-WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]  # ←ここを修正！
+WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 
-def send_slack_message(message: str):
-    payload = {
-        "text": message
-    }
+def send_slack_notification(message):
+    payload = {"text": message}
     response = requests.post(WEBHOOK_URL, json=payload)
-    response.raise_for_status()
+    if response.status_code != 200:
+        raise Exception(f"Request to Slack returned error {response.status_code}, response: {response.text}")
 
-send_slack_message("チケットを確認しました。")
+send_slack_notification("チェック完了：チケットの更新を確認しました。")
